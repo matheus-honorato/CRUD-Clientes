74a75010411a3e9aaddceff18b71af5c1df116a6
Matheus Honorato <matheuswebmw@gmail.com>
2022-09-27 23:50:30 -0300

    Desenvolvido modelo cliente

---
+++ Crud-Clientes/src/br/com/loja/model/Cliente.java
@@ -0,0 +1,66 @@
+package br.com.loja.model;
+
+import java.util.Date;
+
+public class Cliente {
+	
+	private String nome;
+	private String sobrenome;
+	private Long idade;
+	private String genero;
+	private Date dataNascimento;
+	private String senha;
+	private String celular;
+	private String email;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+	public Long getIdade() {
+		return idade;
+	}
+	public void setIdade(Long idade) {
+		this.idade = idade;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public String getCelular() {
+		return celular;
+	}
+	public void setCelular(String celular) {
+		this.celular = celular;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+}
